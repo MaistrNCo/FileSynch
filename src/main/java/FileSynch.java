88d77c16418354987ce9f64a743b365128c8099b
Mikola Maistrenko <uncle.claus@gmail.com>
2017-06-06 21:06:49 +0300

    implemented removing deleted files

--- src/main/java/FileSynch.java
+++ src/main/java/FileSynch.java
@@ -1,9 +1,11 @@
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Iterator;
+import java.util.stream.Stream;
 
 
-public class FileSynch {
+class FileSynch {
     public static void main(String[] args) {
 
         if (args.length != 2) {
@@ -13,7 +15,7 @@ public class FileSynch {
 
         String source = args[0];
         String destination = args[1];
-        if (destination.indexOf(source) > -1) {
+        if (destination.contains(source)) {
             System.out.println("Destination directory is not allowed inside source path!");
             return;
         }
@@ -28,25 +30,34 @@ public class FileSynch {
             Files.walkFileTree(pathIn, new FileVisitor<Path>() {
                 @Override
                 public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                    copyDirectoriesToDest(dir);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                private void copyDirectoriesToDest(Path dir) {
                     Path currTargetPath = Paths.get(dir.toString().replaceFirst(source, destination));
                     try {
                         Files.copy(dir, currTargetPath);
                         System.out.println("copied  directory: " + currTargetPath);
-                    } catch (FileAlreadyExistsException e){
+                    } catch (FileAlreadyExistsException e) {
                         System.out.println("path already exist: " + currTargetPath);
                     } catch (IOException e) {
                         System.out.println("unsuccessful copy of " + dir);
                     }
-                    return FileVisitResult.CONTINUE;
                 }
 
                 @Override
                 public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    copyFilesToDest(file);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                private void copyFilesToDest(Path file) throws IOException {
                     Path currTargetPath = Paths.get(file.toString().replaceFirst(pathIn.toString(), pathOut.toString()));
                     try {
                         Files.copy(file, currTargetPath);
                         System.out.println("copied  file: " + currTargetPath);
-                    } catch (FileAlreadyExistsException e){
+                    } catch (FileAlreadyExistsException e) {
                         if (Files.size(file) != Files.size(currTargetPath)) {
                             Files.copy(file, currTargetPath, StandardCopyOption.REPLACE_EXISTING);
                             System.out.println("file rewritten: " + currTargetPath);
@@ -56,7 +67,6 @@ public class FileSynch {
                     } catch (IOException e) {
                         System.out.println("unsuccessful copy of " + file);
                     }
-                    return FileVisitResult.CONTINUE;
                 }
 
                 @Override
@@ -66,32 +76,33 @@ public class FileSynch {
 
                 @Override
                 public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-
+                    removeDeletedContent(dir);
                     return FileVisitResult.CONTINUE;
                 }
-            });
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    static void findFile(Path path, String fileToFind) {
-        try {
-            Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
-
-                @Override
-                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                    String fileString = file.toAbsolutePath().toString();
-                    if (fileString.endsWith(fileToFind)) {
-                        System.out.println("file found at path: " + file.toAbsolutePath());
-                        return FileVisitResult.TERMINATE;
+                private void removeDeletedContent(Path dir) throws IOException {
+                    Path currTargetPath = Paths.get(dir.toString().replaceFirst(source, destination));
+                    Stream<Path> pathContent = Files.list(currTargetPath);
+                    Iterator<Path> iterator = pathContent.iterator();
+                    while (iterator.hasNext()) {
+                        Path path = iterator.next();
+                        if (Files.notExists(Paths.get(path.toString().replaceFirst(destination, source)))) {
+                            try {
+                                Files.delete(path);
+                                System.out.println("deleted " + path);
+                            } catch (DirectoryNotEmptyException e){
+                                removeDeletedContent(path);
+                                Files.delete(path);
+                                System.out.println("deleted " + path);
+                            }
+                        }
                     }
-                    return FileVisitResult.CONTINUE;
                 }
             });
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
 }
