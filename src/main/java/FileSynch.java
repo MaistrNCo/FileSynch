4ca2899ad60402c6193028efc37e0d893e5ec24d
Mykola Maistrenko <Maistrenko.Mykola@gmail.com>
2017-06-07 16:15:23 +0300

    changed path switching from source to destination

--- src/main/java/FileSynch.java
+++ src/main/java/FileSynch.java
@@ -12,61 +12,36 @@ class FileSynch {
             System.out.println("Wrong parameters amount ");
             return;
         }
-
-        String source = args[0];
-        String destination = args[1];
-        if (destination.contains(source)) {
-            System.out.println("Destination directory is not allowed inside source path!");
-            return;
+        if(Files.isDirectory(Paths.get(args[0]))&&Files.isDirectory(Paths.get(args[1]))){
+            String source = args[0];
+            String destination = args[1];
+            if (destination.contains(source)) {
+                System.out.println("Destination directory is not allowed inside source path!");
+                return;
+            }
+            FileSynch fs = new FileSynch();
+            fs.synchronise(source, destination);
+        } else {
+            System.out.println("");
         }
-
-        synchronise(source, destination);
     }
 
-    private static void synchronise(String source, String destination) {
-        Path pathIn = Paths.get(source);
-        Path pathOut = Paths.get(destination);
+    public  void synchronise(String source, String destination) {
+        Path pathSource = Paths.get(source);
+        Path pathDestination = Paths.get(destination);
         try {
-            Files.walkFileTree(pathIn, new FileVisitor<Path>() {
+            Files.walkFileTree(pathSource, new FileVisitor<Path>() {
                 @Override
                 public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                    removeDeletedContent(dir);
                     copyDirectoriesToDest(dir);
                     return FileVisitResult.CONTINUE;
-                }
-
-                private void copyDirectoriesToDest(Path dir) {
-                    Path currTargetPath = Paths.get(dir.toString().replaceFirst(source, destination));
-                    try {
-                        Files.copy(dir, currTargetPath);
-                        System.out.println("copied  directory: " + currTargetPath);
-                    } catch (FileAlreadyExistsException e) {
-                        System.out.println("path already exist: " + currTargetPath);
-                    } catch (IOException e) {
-                        System.out.println("unsuccessful copy of " + dir);
-                    }
                 }
 
                 @Override
                 public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
                     copyFilesToDest(file);
                     return FileVisitResult.CONTINUE;
-                }
-
-                private void copyFilesToDest(Path file) throws IOException {
-                    Path currTargetPath = Paths.get(file.toString().replaceFirst(pathIn.toString(), pathOut.toString()));
-                    try {
-                        Files.copy(file, currTargetPath);
-                        System.out.println("copied  file: " + currTargetPath);
-                    } catch (FileAlreadyExistsException e) {
-                        if (Files.size(file) != Files.size(currTargetPath)) {
-                            Files.copy(file, currTargetPath, StandardCopyOption.REPLACE_EXISTING);
-                            System.out.println("file rewritten: " + currTargetPath);
-                        } else {
-                            System.out.println("file already exist: " + currTargetPath);
-                        }
-                    } catch (IOException e) {
-                        System.out.println("unsuccessful copy of " + file);
-                    }
                 }
 
                 @Override
@@ -76,22 +51,55 @@ class FileSynch {
 
                 @Override
                 public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-                    removeDeletedContent(dir);
                     return FileVisitResult.CONTINUE;
                 }
 
+                private void copyDirectoriesToDest(Path dir) {
+                    Path relativePath = pathSource.relativize(dir);
+                    Path currDestinationPath = pathDestination.resolve(relativePath);
+                    try {
+                        Files.copy(dir, currDestinationPath);
+                        System.out.println("copied  directory: " + currDestinationPath);
+                    } catch (FileAlreadyExistsException e) {
+                        System.out.println("path already exist: " + currDestinationPath);
+                    } catch (IOException e) {
+                        System.out.println("unsuccessful copy of " + dir);
+                    }
+                }
+
+                private void copyFilesToDest(Path file) throws IOException {
+                    Path relativePath = pathSource.relativize(file);
+                    Path currDestinationPath = pathDestination.resolve(relativePath);
+                    try {
+                        Files.copy(file, currDestinationPath);
+                        System.out.println("copied  file: " + currDestinationPath);
+                    } catch (FileAlreadyExistsException e) {
+                        if (Files.size(file) != Files.size(currDestinationPath)) {
+                            Files.copy(file, currDestinationPath, StandardCopyOption.REPLACE_EXISTING);
+                            System.out.println("file rewritten: " + currDestinationPath);
+                        } else {
+                            System.out.println("file already exist: " + currDestinationPath);
+                        }
+                    } catch (IOException e) {
+                        System.out.println("unsuccessful copy of " + file);
+                    }
+                }
+
                 private void removeDeletedContent(Path dir) throws IOException {
-                    Path currTargetPath = Paths.get(dir.toString().replaceFirst(source, destination));
-                    Stream<Path> pathContent = Files.list(currTargetPath);
-                    Iterator<Path> iterator = pathContent.iterator();
+                    Path relativePath = pathSource.relativize(dir);
+                    Path currDestinationPath = pathDestination.resolve(relativePath);
+                    Stream<Path> destPathContent = Files.list(currDestinationPath);
+                    Iterator<Path> iterator = destPathContent.iterator();
                     while (iterator.hasNext()) {
                         Path path = iterator.next();
-                        if (Files.notExists(Paths.get(path.toString().replaceFirst(destination, source)))) {
+                        Path sourcePath = pathDestination.relativize(path);
+                        sourcePath = pathSource.resolve(sourcePath);
+                        if (Files.notExists(sourcePath)) {
                             try {
                                 Files.delete(path);
                                 System.out.println("deleted " + path);
                             } catch (DirectoryNotEmptyException e){
-                                removeDeletedContent(path);
+                                removeDeletedContent(sourcePath);
                                 Files.delete(path);
                                 System.out.println("deleted " + path);
                             }
